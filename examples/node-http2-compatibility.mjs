62a3cae0472bbcea9f6250c761857abd38f40f7f
Yoshiaki Kawazu <kawazzz@gmail.com>
2020-07-08 14:21:38 +0900

    Add examples

---
+++ examples/node-http2-compatibility.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import http2 from 'http2'
+
+Promise.all([
+  new Promise((ok,ng,_='')=>http2.connect('https://oreore.net').request({':path':'/key.pem'}).on('data',d=>_+=d).on('end',()=>ok(_)).on('error',ng).end()),
+  new Promise((ok,ng,_='')=>http2.connect('https://oreore.net').request({':path':'/cert.pem'}).on('data',d=>_+=d).on('end',()=>ok(_)).on('error',ng).end()),
+])
+  .then(([key,cert])=>new Promise((ok,ng)=>{
+    const port = process.argv[2] ?? 8443;
+    http2.createSecureServer({key, cert, allowHTTP1:true}, (req, res) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({req:{
+        ...["method", "url", "httpVersion", "headers"].reduce((o,k)=>Object.assign(o, {[k]:req[k]}), {}),
+        ...{socket:['servername','alpnProtocol'].reduce((o,k)=>Object.assign(o, {[k]:req.socket[k]}), {})},
+      }}))
+    })
+      .on('error', ng)
+      .on('close', ok)
+      .on('listening', ()=>['localhost','sub.localhost','lo','sub.lo','sub','127-0-0-1.ip', 'sub-127-0-0-1.ip'].map(s=>console.log(`https://${s}.oreore.net:${port}`)))
+      .listen(port)
+  }))
+  .then(r=>console.log(r))
+  .catch(e=>console.error(e))
---
+++ examples/node-https.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import https from 'https'
+
+Promise.all([
+  new Promise((ok,ng,_='')=>https.request('https://oreore.net/key.pem',res=>res.on('data',d=>_+=d).on('end',()=>ok(_))).on('error',ng).end()),
+  new Promise((ok,ng,_='')=>https.request('https://oreore.net/cert.pem',res=>res.on('data',d=>_+=d).on('end',()=>ok(_))).on('error',ng).end()),
+])
+  .then(([key,cert])=>new Promise((ok,ng)=>{
+    const port = process.argv[2] ?? 8443;
+    https.createServer({key, cert}, (req, res) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({req:{
+        ...["method", "url", "httpVersion", "headers"].reduce((o,k)=>Object.assign(o, {[k]:req[k]}), {}),
+        ...{socket:['servername','alpnProtocol'].reduce((o,k)=>Object.assign(o, {[k]:req.socket[k]}), {})},
+      }}))
+    })
+      .on('error', ng)
+      .on('close', ok)
+      .on('listening', ()=>['localhost','sub.localhost','lo','sub.lo','sub','127-0-0-1.ip', 'sub-127-0-0-1.ip'].map(s=>console.log(`https://${s}.oreore.net:${port}`)))
+      .listen(port)
+  }))
+  .then(r=>console.log(r))
+  .catch(e=>console.error(e))
